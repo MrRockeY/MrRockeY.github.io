973dbffda0db727607f8886dc29f0823b1bc6041
MrRockeY <mrrockeyx@gmail.com>
2025-04-20 10:29:13 +0500

    Fix: Update routing for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
   ],
-  base: "/MrRockeY.github.io/",
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
